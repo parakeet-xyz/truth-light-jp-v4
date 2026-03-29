426e3f98043aebaaf323891ca9131b02fb86484a
psy-yumekawa <psy.yumekawa@proton.me>
2026-03-29 10:30:02 +0900

    Fix: index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,21 +7,37 @@
         <p class="w-[60%] mx-auto text-lg md:text-xl text-[#25343F]/70 tracking-tight text-center text-balance leading-50">Truth Lightはあらゆる精神活性物質の作用・副作用・危険性<br>そして薬物依存症からの回復の方法まで正しい情報を提供します。</p>
       </div>
       <section class="">
-        <h2 class="text-xl text-center">規制情報、ハームリダクション、薬物依存症からの回復についてAIに聞く</h2>
-        <div class="flex gap-4">
-          <button type="button" class="inline-flex text-lg">テキストで聞く</button>
-          <button type="button" class="inline-flex text-lg">構造式で聞く</button>         
+        <h2 class="mb-12 text-xl text-center">規制情報、ハームリダクション、薬物依存症からの回復についてAIに聞く</h2>
+        <div class="flex gap-4 justify-center pb-10">
+          <button
+            v-for="t in tabs"
+            :key="t.key"
+            type="button"
+            class="inline-flex p-1 text-lg rounded-lg border hover:text-[#EAEFEF] hover:bg-[#FF9B51] hover:border-[#FF9B51]"
+            :class="activeTab === t.key
+              ? 'text-[#EAEFEF] bg-[#FF9B51] border-[#FF9B51]'
+              : 'text-[#25343F] bg-[#BFC9D1]/40 border-[#BFC9D1]/60'
+            "
+            @click="activeTab = t.key"
+          >
+            {{t.label}}
+          </button>
         </div>
-        <input type="text" id="aiTextSearch" name="aiTextSearch" placeholder="ex. MDPVって規制されてる？"
-          class="border-2 border"
-        />
+        <div
+          v-if="activeTab === 'text'"
+        >
+          <input type="text" id="aiTextSearch" name="aiTextSearch" placeholder="ex. MDPVって規制されてる？"
+            class="w-full h-12 px-4 text-lg border-2 rounded-xl placeholder:text-lg"
+          />
+        </div>
+        <div
+          v-else
+        >
+            <ClientOnly>
+              <ContentKetcherFrame />
+            </ClientOnly>          
+        </div>
       </section>
-      <div>
-        <h2>構造式検索</h2>
-          <ClientOnly>
-            <ContentKetcherFrame />
-          </ClientOnly>          
-      </div>
     </section>
 
   </main>
@@ -41,5 +57,13 @@ useSeoMeta({
   title: appConfig.truthlight?.site?.name ?? `Truth Light`,
   description: appConfig.truthlight?.site?.description ?? ``,
 })
+
+// Tabs
+const tabs = [
+  { key: "text", label: "テキストで聞く" },
+  { key: "structure", label: "構造式で聞く"}
+] as const
+
+const activeTab = ref<(typeof tabs)[number]["key"]>("text")
 
 </script>
