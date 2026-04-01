a7d859078da4992f1c2be6b963557d497c72b27b
psy-yumekawa <psy.yumekawa@proton.me>
2026-04-01 15:05:47 +0900

    Fix: nuxt content

--- pages/docs/[...slug].vue
+++ pages/docs/[...slug].vue
@@ -1,6 +1,58 @@
 <template>
-  <main class="prose">
-    <ContentRenderer v-if="page" :value="page" />
+  <main
+    id="__nuxtContent"
+  >
+    <div class="grid place-items-center w-[100%] h-[250px] bg-[#25343F]">
+      <div class="mx-auto max-w-6xl">
+        <h1 class="custom-font-bold text-4xl tracking-wider text-[#EAEFEF]">
+          {{ page?.title ?? '' }}
+        </h1>
+        <p></p>
+      </div>
+    </div>
+    <div
+      class="prose
+      mx-auto max-w-6xl md:flex md:flex-row-reverse"
+    >
+      <!-- TOC -->
+      <aside
+        v-if="page?.body?.toc?.links?.length"
+        class="w-full mx-6
+        md:w-1/4"
+        aria-label="Table of contents"
+      >
+        <nav class="rounded-lg border p-4">
+          <p class="mb-3 text-sm font-semibold">目次</p>
+
+          <ul class="space-y-2 text-sm">
+            <li v-for="link in page.body.toc.links" :key="link.id">
+              <a :href="`#${link.id}`" class="block hover:underline">
+                {{ link.text }}
+              </a>
+
+              <ul
+                v-if="link.children?.length"
+                class="mt-2 ml-4 space-y-1"
+              >
+                <li v-for="child in link.children" :key="child.id">
+                  <a :href="`#${child.id}`" class="block hover:underline">
+                    {{ child.text }}
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+
+      <!-- 本文 -->
+      <article
+        class="flex-1"
+      >
+        <ContentRenderer v-if="page" :value="page" />  
+      </article>
+    </div>
+
   </main>
 </template>
 
@@ -25,6 +77,7 @@ if (!page.value) {
 useSeoMeta({
   title: () => page.value?.seo?.title ?? page.value?.title,
   description: () => page.value?.seo?.description ?? page.value?.description,
+  ogDescription: () => page.value?.seo?.description ?? page.value?.description,
 })
 
 // seo.meta / seo.link みたいな「配列系」は useHead 側に流す
--- pages/substances/index.vue
+++ pages/substances/index.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import ContentSubstancesTable from '~/components/content/SubstancesTable.client.vue'
+
 definePageMeta({ layout: "substances" })
 
-
+useSeoMeta({
+  title: `NPSデータベース(α版)`,
+  description: `現在日本で規制されている薬物やNPS(新規精神活性物質)のデータベース`,
+  ogDescription: `現在日本で規制されている薬物やNPS(新規精神活性物質)のデータベース`,
+})
 
 </script>
 
@@ -10,7 +15,7 @@ definePageMeta({ layout: "substances" })
   <ClientOnly>
     <ContentSubstancesTable />
     <template #fallback>
-      <div class="text-slate-400 text-sm px-6 pt-5">Loading…</div>
+      <div class="text-base text-center pt-12">Loading…</div>
     </template>
   </ClientOnly>
 </template>
